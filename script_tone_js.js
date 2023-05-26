d2fc592db12cf0d3086179b0ed790f3bf9f4bf89
mariusono <59313470+mariusono@users.noreply.github.com>
2023-05-26 10:56:23 +0200

    newBranch

--- script_tone_js.js
+++ script_tone_js.js
@@ -34,14 +34,37 @@ let sonifiedObjects = {};
 
 
 // // Try to make a global loop to go through each sonified object at a time.. 
-let loopGlobal = new Tone.Loop(loopStep, "1n");  // '1n' here sets the speed of our loop -- every 1th note
+// let loopGlobal = new Tone.Loop(loopStep, "1n");  // '1n' here sets the speed of our loop -- every 1th note
 
 count = 0;
+
+let flagAllSounds = false;
+
+const checkbox_sounds = document.getElementById("checkbox_sounds");
+
+checkbox_sounds.addEventListener("change", () => {
+    flagAllSounds = !flagAllSounds;
+    console.log(flagAllSounds);
+});
+
+let loopGlobal;
+if (flagAllSounds)
+{
+    loopGlobal = new Tone.Loop(loopStep, '16n');  // '1n' here sets the speed of our loop -- every 1th note
+}
+else
+{
+    loopGlobal = new Tone.Loop(loopStep, "1n");  // '1n' here sets the speed of our loop -- every 1th note
+}
+
+let intervalVal = loopGlobal.interval;
+
 function loopStep(time){
     let sonifiedObjects_keys = Object.keys(sonifiedObjects);
     let objectsPlaying = [];
+    let objectsNotPlaying = [];
 
-    console.log(sonifiedObjects);
+    // console.log(sonifiedObjects);
 
     for (let i = 0; i<sonifiedObjects_keys.length;i++)
     {
@@ -50,30 +73,60 @@ function loopStep(time){
             // objectsPlaying[sonifiedObjects_keys[i]] = sonifiedObjects[sonifiedObjects_keys[i]];
             objectsPlaying.push(sonifiedObjects[sonifiedObjects_keys[i]]);
         }
+        else
+        {
+            objectsNotPlaying.push(sonifiedObjects[sonifiedObjects_keys[i]]);
+        }
     }
 
-    console.log(objectsPlaying);
-
-    if (objectsPlaying.length > 0)
+    // console.log(objectsPlaying);
+    if (flagAllSounds)
     {
-        if (count>=objectsPlaying.length) {count = 0;};
-
-        if (objectsPlaying[count] instanceof droneSonification)
+        for (let i = 0;i<objectsPlaying.length;i++)
         {
-            objectsPlaying[count].envelope.triggerAttackRelease('1n',time);
+            if (objectsPlaying[i] instanceof droneSonification)
+            {
+                objectsPlaying[i].envelope.triggerAttack();
+            }
+            else if (objectsPlaying[i] instanceof synthLoopSonification)
+            {
+                objectsPlaying[i].loop.start(); // start the synthSonification loop
+            }
         }
-        else if (objectsPlaying[count] instanceof synthLoopSonification)
+        for (let i = 0;i<objectsNotPlaying.length;i++)
         {
-            objectsPlaying[count].loop.start(); // start the synthSonification loop
-            objectsPlaying[count].loop.stop('+1n'); // close it at a future time.. 
+            if (objectsNotPlaying[i] instanceof droneSonification)
+            {
+                objectsNotPlaying[i].envelope.triggerRelease();
+            }
+            else if (objectsNotPlaying[i] instanceof synthLoopSonification)
+            {
+                objectsNotPlaying[i].loop.stop(); // start the synthSonification loop
+            }
+        }        
+    }
+    else
+    {
+        console.log('here');
+        if (objectsPlaying.length > 0)
+        {
+            if (count>=objectsPlaying.length) {count = 0;};
+    
+            if (objectsPlaying[count] instanceof droneSonification)
+            {
+                // objectsPlaying[count].envelope.triggerAttackRelease('1n',time);
+                objectsPlaying[count].envelope.triggerAttackRelease(String(intervalVal),time);
+            }
+            else if (objectsPlaying[count] instanceof synthLoopSonification)
+            {
+                objectsPlaying[count].loop.start(); // start the synthSonification loop
+                // objectsPlaying[count].loop.stop('+1n'); // close it at a future time.. 
+                objectsPlaying[count].loop.stop('+'+String(intervalVal)); // close it at a future time.. 
+            }
+            count = count + 1;
         }
-        count = count + 1;
     }
 }
-
-
-
-
 
 
 //attach a click listener to a play button
@@ -99,6 +152,29 @@ button_2.addEventListener("click", async () => {
     Tone.Transport.stop(); // this just stops the master time.. 
  
 });
+
+
+// ADD SLIDER FUNCTIONS
+
+function setGain(v) {
+    let gainVal = linearMapping(-30.0, 10.0, 0, 10000, v); // db linear Scale
+    let gainVal_amp = 10 ** (gainVal / 20);
+    if (gainVal_amp < 0.0316 + 0.0001) { // equivalent of -30 dB + 0.0001
+        gainVal_amp = 0;
+    }
+    document.getElementById('Gain').innerText = parseFloat(gainVal).toFixed(4);
+    // gainNode.gain.value = gainVal;
+    gainNode.gain.rampTo(gainVal_amp, 0.1);
+}
+
+function setLoopTime(v) {
+    intervalVal = linearMapping(0.1, 5.0, 0, 10000, v); // db linear Scale
+
+    document.getElementById('Interval').innerText = parseFloat(intervalVal).toFixed(4);
+    // gainNode.gain.value = gainVal;
+    loopGlobal.interval = intervalVal;
+}
+
 
 
 // Clear console after load.. 
